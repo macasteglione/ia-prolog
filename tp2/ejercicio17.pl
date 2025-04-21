b87fda8150e737113615b3499e15571ab3a469f6
Matias Casteglione <matiascasteglione2002@gmail.com>
2025-04-21 04:18:07 +0000

    ejercicio17

--- tp2/ejercicio17.pl
+++ tp2/ejercicio17.pl
@@ -1,1 +1,24 @@
+llamar_bomberos :- fuego.
 
+fuego :- humo.
+fuego :- alarma_incendio_activada.
+fuego :- calor_intenso, noche.
+
+llamar_policia :- llamar_bomberos.
+llamar_policia :- ladrones.
+llamar_policia :- cerrado, ventana_abierta.
+
+ladrones :- movimiento, cerrado.
+
+publico :- salon4.
+publico :- salon5.
+
+movimiento :- sensor_mov_salon1_s1, sensor_mov_salon1_s2.
+movimiento :- sensor_mov_salon2_s1, sensor_mov_salon2_s2.
+movimiento :- sensor_mov_salon3_s1, sensor_mov_salon3_s2.
+movimiento :- sensor_mov_salon4.
+movimiento :- sensor_mov_salon5.
+
+cerrado:- domingo.
+
+activar_grupo_electrogeno :- corte_de_luz.
