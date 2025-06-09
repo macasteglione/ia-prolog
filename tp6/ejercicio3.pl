1e75de0426399547b3c3d68c2022b123009e616b
macasteglione <matiascasteglione2002@gmail.com>
2025-06-08 23:35:11 -0300

    arreglo de las oraciones del ejercicio 3

--- tp6/ejercicio3.pl
+++ tp6/ejercicio3.pl
@@ -1,28 +1,43 @@
-oracion --> sujeto(Num, Gen), verbo(Num), complemento.
-sujeto(Num, Gen) --> articulo(Num, Gen), sustantivo(Num, Gen).
-complemento --> articulo(Num2, Gen2), sustantivo(Num2, Gen2).
+oracion --> sujeto(Num, Gen, Suj), verbo(Num, V), complemento(Obj), {accion(Suj, V, Obj)}.
+sujeto(Num, Gen, Suj) --> articulo(Num, Gen), sustantivo(Num, Gen, Suj).
+complemento(Obj) --> articulo(Num2, Gen2), sustantivo(Num2, Gen2, Obj).
 
 articulo(singular, masculino) --> [el].
-articulo(plural, masculino)   --> [los].
-articulo(singular, femenino)  --> [la].
-articulo(plural, femenino)    --> [las].
+articulo(plural, masculino) --> [los].
+articulo(singular, femenino) --> [la].
+articulo(plural, femenino) --> [las].
 articulo(singular, masculino) --> [un].
-articulo(singular, femenino)  --> [una].
+articulo(singular, femenino) --> [una].
 
-sustantivo(singular, masculino) --> [gato].
-sustantivo(plural, masculino)   --> [gatos].
-sustantivo(singular, femenino)  --> [profesora].
-sustantivo(plural, femenino)    --> [profesoras].
-sustantivo(singular, masculino) --> [profesor].
-sustantivo(plural, masculino)   --> [profesores].
-sustantivo(singular, femenino)  --> [niña].
-sustantivo(singular, masculino) --> [perro].
-sustantivo(singular, femenino)  --> [pelota].
-sustantivo(singular, masculino) --> [libro].
-sustantivo(singular, masculino) --> [pescado].
+sustantivo(singular, masculino, gato) --> [gato].
+sustantivo(plural, masculino, gatos) --> [gatos].
+sustantivo(singular, femenino, profesora) --> [profesora].
+sustantivo(plural, femenino, profesoras) --> [profesoras].
+sustantivo(singular, masculino, profesor) --> [profesor].
+sustantivo(plural, masculino, profesores) --> [profesores].
+sustantivo(singular, femenino, niña) --> [niña].
+sustantivo(plural, femenino, niñas) --> [niñas].
+sustantivo(singular, masculino, niño) --> [niño].
+sustantivo(plural, masculino, niños) --> [niños].
+sustantivo(singular, masculino, perro) --> [perro].
+sustantivo(plural, masculino, perros) --> [perros].
+sustantivo(singular, femenino, pelota) --> [pelota].
+sustantivo(singular, masculino, libro) --> [libro].
+sustantivo(plural, masculino, libros) --> [libros].
+sustantivo(singular, masculino, pescado) --> [pescado].
+sustantivo(plural, masculino, pescados) --> [pescados].
 
-verbo(singular) --> [come].
-verbo(plural)   --> [comen].
-verbo(singular) --> [lee].
-verbo(plural)   --> [leen].
-verbo(singular) --> [muerde].
+verbo(singular, come) --> [come].
+verbo(plural, comen) --> [comen].
+verbo(singular, lee) --> [lee].
+verbo(plural, leen) --> [leen].
+verbo(singular, muerde) --> [muerde].
+
+% Verbos que pueden hacer ciertos sujetos con ciertos objetos
+accion(gato, come, pescado).
+accion(gato, come, libro).
+accion(niña, lee, libro).
+accion(perro, muerde, pelota).
+accion(profesora, lee, libro).
+accion(profesor, lee, libro).
+accion(profesores, leen, libros).
